d3abb7c77ae2591a58762750bb287c7a5b72c0a7
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2024-01-18 15:18:32 +0000

    Update code for benchmark

--- test_zarr_write.py
+++ test_zarr_write.py
@@ -5,48 +5,157 @@ import zarr
 import argparse
 import configparser
 import logging
+import pandas as pd
+import numpy as np
+import multiprocessing as mp
 from functools import partial
 from pathlib import Path
-import multiprocessing as mp
+from rich.progress import track
 
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.StreamHandler()
 logger.addHandler(handler)
 
 
-def create_random_file(file_name: str, s3: s3fs.S3FileSystem, size: int = 1024):
-    logger.info(f"Create file {file_name}")
-    with s3.open(file_name, "wb") as f:
-        logger.debug("Opened")
-        f.write(os.urandom(size))
+class Timer:
+    def __enter__(self):
+        self.start = time.time()
 
-    logger.info(f"Done file {file_name}")
-    return
-
-
-def write_zarr_data(name: str, key: str, secret: str, url: str):
-    s3 = s3fs.S3FileSystem(anon=False, key=key, secret=secret, endpoint_url=url)
-    store = zarr.storage.FSStore(f"s3://{name}/my_file.zarr", fs=s3)
-    root = zarr.group(store=store, overwrite=True)
-    s = time.time()
-    for i in range(10):
-        z = root.zeros(f"{i}", shape=(30000), dtype="float32")
-        z[:] = 42.0
-    e = time.time()
-    print(f"\tWrite time {e-s:.2f}")
-    store.close()
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.end = time.time()
+        self.duration = self.end - self.start
 
 
-def read_zarr_data(name: str, key: str, secret: str, url: str):
-    s3 = s3fs.S3FileSystem(anon=False, key=key, secret=secret, endpoint_url=url)
-    store = zarr.storage.FSStore(f"s3://{name}/my_file.zarr", fs=s3)
-    root = zarr.open(store=store)
-    s = time.time()
-    for i in range(10):
-        arr = root[f"{i}"][:]
-    e = time.time()
-    print(f"\tRead time {e-s:.2f}")
+def write_random_file(file_name: str, s3: s3fs.S3FileSystem, size: int = 1024):
+    logger.debug(f"Create file {file_name}")
+
+    timer = Timer()
+    with timer:
+        with s3.open(file_name, "wb") as f:
+            f.write(os.urandom(size))
+
+    logger.debug(f"Done writing file {file_name}")
+    return timer.duration
+
+
+def read_file(file_name: str, s3: s3fs.S3FileSystem):
+    logger.debug(f"Reading file {file_name}")
+    timer = Timer()
+    with timer:
+        with s3.open(file_name, "rb") as f:
+            contents = f.read()
+    logger.debug(f"Done reading file {file_name}")
+    return timer.duration
+
+
+def write_test(
+    s3: s3fs.S3FileSystem,
+    bucket_name: str,
+    file_size: int = 1024,
+    n_samples: int = 10,
+    n_workers: int = 4,
+):
+    logger.info(f"Running write test with {n_samples} samples and {n_workers} workers")
+    _do_write = partial(write_random_file, s3=s3, size=file_size)
+    file_names = [f"{bucket_name}/test_file_{i}.bin" for i in range(n_samples)]
+
+    timer = Timer()
+    with timer:
+        with mp.Pool(n_workers) as pool:
+            times = list(track(pool.imap(_do_write, file_names), total=len(file_names)))
+            times = np.array(times)
+
+    total_duration = timer.duration
+    return dict(total_duration=total_duration, times=times)
+
+
+def read_test(
+    s3: s3fs.S3FileSystem,
+    bucket_name: str,
+    n_samples: int = 10,
+    n_workers: int = 4,
+):
+    logger.info(f"Running read test with {n_samples} samples and {n_workers} workers")
+    _do_read = partial(read_file, s3=s3)
+    file_names = [f"{bucket_name}/test_file_{i}.bin" for i in range(n_samples)]
+    file_names = list(reversed(file_names))
+
+    timer = Timer()
+    with timer:
+        with mp.Pool(n_workers) as pool:
+            times = list(track(pool.imap(_do_read, file_names), total=len(file_names)))
+            times = np.array(times)
+
+    total_duration = timer.duration
+    return dict(total_duration=total_duration, times=times)
+
+
+def do_test(
+    s3: s3fs.S3FileSystem, name: str, n_samples: int, n_workers: int, file_size: int
+):
+    total_size = file_size * n_samples
+
+    result = write_test(
+        s3, name, file_size=file_size, n_samples=n_samples, n_workers=n_workers
+    )
+
+    total_duration = result["total_duration"]
+    times = result["times"]
+    avg_write_time = times.mean()
+    throughput = total_size / total_duration
+
+    write_result = {
+        "write_time_total": total_duration,
+        "write_time_avg": avg_write_time,
+        "write_throughput": throughput,
+    }
+
+    logger.info(f"Average write time {avg_write_time}")
+    logger.info(f"Total Time taken {total_duration}")
+    logger.info(f"Througput (bytes/s) {throughput}")
+
+    time.sleep(1)
+
+    result = read_test(s3, name, n_samples=n_samples, n_workers=n_workers)
+
+    total_duration = result["total_duration"]
+    times = result["times"]
+    avg_read_time = times.mean()
+    throughput = total_size / total_duration
+
+    read_result = {
+        "read_time_total": total_duration,
+        "read_time_avg": avg_read_time,
+        "read_throughput": throughput,
+    }
+
+    logger.info(f"Time taken {total_duration}")
+    logger.info(f"Average read time {avg_read_time}")
+    logger.info(f"Througput (bytes/s) {throughput}")
+
+    results = {}
+    results.update(write_result)
+    results.update(read_result)
+    return results
+
+
+def grid_scan(s3: s3fs.S3FileSystem, name: str, output_file: str):
+    n_samples = 100
+    file_size = 1024
+
+    n_workers_scan = [1, 2, 4, 6, 8, 10, 12]
+
+    results = []
+    for n_workers in n_workers_scan:
+        metrics = do_test(s3, name, n_samples, n_workers, file_size)
+        metrics["n_workers"] = n_workers
+        metrics["n_samples"] = n_samples
+        metrics["file_size"] = file_size
+        results.append(metrics)
+
+    df = pd.DataFrame(results)
+    df.to_csv(output_file)
 
 
 def main():
@@ -57,6 +166,7 @@ def main():
 
     parser.add_argument("config_file")
     parser.add_argument("bucket_name")
+    parser.add_argument("output_file")
     args = parser.parse_args()
 
     config = configparser.ConfigParser()
@@ -68,33 +178,13 @@ def main():
     secret = config["secret_key"]
     url = f"https://{config['host_base']}"
     name = args.bucket_name
-    n_samples = 100
-    n_workers = 1
 
     s3 = s3fs.S3FileSystem(anon=False, key=key, secret=secret, endpoint_url=url)
-    _do_write = partial(create_random_file, s3=s3)
-    file_names = [f"{name}/test_file_{i}.bin" for i in range(n_samples)]
-
-    with mp.Pool(n_workers) as pool:
-        s = time.time()
-        list(pool.imap(_do_write, file_names))
-        # _do_write(file_names[0])
-        e = time.time()
-        logger.info(f"Time taken {e - s}")
-
-    # path = Path("large_files")
-    # path.mkdir(exist_ok=True)
-    # for i in range(10):
-    #     create_random_file(path / f"test_file_{i}.dump", 1024 * 100000)
-
-    # logging.info("Test writing")
-    # write_zarr_data(name, key, secret, url)
-    # logging.info("Done writing")
-
-    # logging.info("Test reading")
-    # read_zarr_data(name, key, secret, url)
-    # logging.info("Done reading")
+    grid_scan(s3, name, args.output_file)
 
 
 if __name__ == "__main__":
+    # Important! Must change multiprocessing spawning method or s3fs will hang in new processes.
+    # See https://s3fs.readthedocs.io/en/latest/index.html?highlight=multiprocessing#multiprocessing
+    mp.set_start_method("spawn", force=True)
     main()
